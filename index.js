6f8450b91cbf696e9f76614f2cc32e42bff1917d
ruananqing <1536788454@qq.com>
2018-01-22 22:04:37 +0800

    v0.0.4
    beautify some codes

--- index.js
+++ index.js
@@ -3,46 +3,20 @@ module.exports = function unequalProbabilityRandom(options) {
 	let values = Object.values(options);
 	let len = keys.length;
 	let totalProb = 0;
-	// let executedStr = '';
-	let result = '';
-
-	function findLongestValueLength(array) {
-		let len = array.length;
-		let valueLengths = [];
-
-		for (let i = 0; i < len; i++) {
-			valueLengths[i] = array[i].toString().length;
-		}
-
-		function findMaximum(arr) {
-			let maximum = arr[0];
-			arr.forEach(element = >{
-				if (element > maximum) {
-					maximum = element;
-				}
-			});
-			return maximum;
-		}
-
-		let longestValueLength = findMaximum(valueLengths);
-
-		return longestValueLength;
-	}
-
 	let powerNum = findLongestValueLength(values);
+	let coefficient = 10 ** powerNum;	//a number that makes decimals to be integers, in order to avoid the situation that some decimals' summation is not exactly equal to 1 when they are added
 
-	let coefficient = 10 * *powerNum;
-
-	values.forEach(prob = >{
+	values.forEach(prob => {
 		totalProb += prob * coefficient;
-	});
-
-	if (totalProb != 1 * coefficient) {
+    });
+    
+	if (totalProb != 1 * coefficient) {	//to judge whether the total probabilities(options' values' summation) is equal to 1
 		throw new Error("the options' total probability is unequal to 1");
 		return null;
 	}
 
-	let probs = [0];
+	let probs = [0];	//set the breakpoints for each probability area
+
 	for (let i = 1; i < len + 1; i++) {
 		probs[i] = values[i - 1] + probs[i - 1];
 	}
@@ -51,7 +25,9 @@ module.exports = function unequalProbabilityRandom(options) {
 	let minusLeft = parseFloat(keys[0].slice(0, minusCharIndex));
 	let minusRight = parseFloat(keys[0].slice(minusCharIndex + 1));
 
-	if (!isNaN(minusLeft) && !isNaN(minusRight)) {
+	//to judge the options' keys for different result's returning
+	if (!isNaN(minusLeft) && !isNaN(minusRight)) {	//if the options' keys are areas made by numbers, return float numbers
+		
 		let boundarys = [];
 
 		for (let i = 0; i < len + 1; i++) {
@@ -63,53 +39,73 @@ module.exports = function unequalProbabilityRandom(options) {
 				boundarys[i] = parseFloat(keys[i - 1].slice(minusCharIndex + 1));
 			}
 		}
-
+		
 		return function() {
+
 			let probability = Math.random();
-
+			let result = '';
 			let executedStr = '';
-
+			
 			for (let i = 0; i < len; i++) {
 				executedStr += `
-				if (probs[$ {
-					i
-				}] < probability && probability <= probs[$ {
-					i + 1
-				}]) {
-					result = boundarys[$ {
-						i
-					}] + Math.random() * (boundarys[$ {
-						i + 1
-					}] - boundarys[$ {
-						i
-					}]);
-				}`
+				if (probs[${i}] < probability && probability <= probs[${i + 1}]) {
+					result = boundarys[${i}] + Math.random() * (boundarys[${i + 1}] - boundarys[${i}]);
+				}
+				`
 			}
+
 			eval(executedStr);
+			
 			return result;
 		}
 
 	} else {
-		return function() {
+
+		return function() {	//if the options' keys are strings(or some single number), return strings
+
 			let probability = Math.random();
-
+			let result = '';
 			let executedStr = '';
-
+			
 			for (let i = 0; i < len; i++) {
 				executedStr += `
-				if (probs[$ {
-					i
-				}] < probability && probability <= probs[$ {
-					i + 1
-				}]) {
-					result = keys[$ {
-						i
-					}];
-				}`
+				if (probs[${i}] < probability && probability <= probs[${i + 1}]) {
+					result = keys[${i}];
+				}
+				`
 			}
+
 			eval(executedStr);
+
 			return result;
 		}
 	}
+	
+}
+
+function findLongestValueLength(array) {
+
+	let len = array.length;
+	let valueLengths = [];
+
+	for (let i = 0; i < len; i++) {
+		valueLengths[i] = array[i].toString().length;
+	}
+
+	return findMaximum(valueLengths);
+
+}
+
+function findMaximum(array) {
+
+	let maximum = array[0];
+
+	array.forEach(element => {
+		if (element > maximum) {
+			maximum = element;
+		}
+	});
+
+	return maximum;
 
 }
--- readme.md
+++ readme.md
@@ -1,28 +1,47 @@
-# Usage:
+# <font color="007acc">Introduction:</font>
+<font color="red" size = "3px">unequalprobabilityrandom</font>
+is a Node.js package for returning unequal probability numbers or strings
+
+# <font color="e39d38">Installation:</font>
+```shell
+npm install unequalprobabilityrandom
+```
+
+# <font color="298a48">Usage:</font>
+
+## 1.For returning unequal random strings:
+
 ```js
 const uprandomFun = require('unequalprobabilityrandom');
 
 // when options like this,  you will get the random string(options' keys) with unequal probability
-// note: the summation(total probability) of the options' values(probabilitys) should be a 1 when each added
-let options1 = {
+// note: the summation(total probability) of the options' values(probabilitys) should be a 1 when each value added
+let options = {
     'a': 0.1,
     'b': 0.2,
     'c': 0.3,
     'd': 0.4
 };
 
-const uprandom1 = uprandomFun(options1);
+const uprandom = uprandomFun(options);
 
-uprandom1(); //return 'a' or 'b' or 'c' or 'd' with unequal probability 10%, 20%, 30%, 40%
+uprandom(); //return 'a' or 'b' or 'c' or 'd' with unequal probability 10%, 20%, 30%, 40%
+```
+
+## Test for upradom()'s strings:
+
+```js
+let times = 100000; //a bigger times means closer probabilities
 
 let Atimes = 0;
 let Btimes = 0;
 let Ctimes = 0;
 let Dtimes = 0;
-let times1 = 100000;
 
-for (let i = 0; i < times1; i++) {
-    let random = uprandom1();
+for (let i = 0; i < times; i++) {
+
+    let random = uprandom();
+
     if (random == 'a') {
         Atimes++;
     }
@@ -38,16 +57,21 @@ for (let i = 0; i < times1; i++) {
 }
 
 console.log({
-    'a': Atimes/times1,
-    'b': Btimes/times1,
-    'c': Ctimes/times1,
-    'd': Dtimes/times1
+    'a': Atimes/times,
+    'b': Btimes/times,
+    'c': Ctimes/times,
+    'd': Dtimes/times
 });     //similar to 0.1, 0.2, 0.3, 0.4
+```
 
+## 2.For returning unequal random numbers in different areas:
+
+```js
+const uprandomFun = require('unequalprobabilityrandom');
 
 // when options like this,  you will get the random float number(numbers in options' keys' area: 20-9999) with unequal probability
-// note: the summation(total probability) of the options' values(probabilitys) should be a 1 when each added
-let options2 = {
+// note: the summation(total probability) of the options' values(probabilitys) should be a 1 when each value added
+let options = {
     '20-30': 0.05,
     '30-60': 0.3,
     '60-80': 0.22,
@@ -55,19 +79,26 @@ let options2 = {
     '85-9999': 0.35
 }
 
-const uprandom2 = uprandomFun(options2);
+const uprandom = uprandomFun(options);
 
-uprandom2(); //return a float number(in 20-9999) with unequal probability in different area
+uprandom(); //return a float number(in 20-9999) with unequal probability in different area
+```
+
+## Test for upradom()'s numbers:
+
+```js
+let times = 100000; //a bigger times means closer probabilities
 
 let Area1times = 0;
 let Area2times = 0;
 let Area3times = 0;
 let Area4times = 0;
 let Area5times = 0;
-let times2 = 100000;
 
-for (let i = 0; i < times2; i++) {
-    let random = uprandom2();
+for (let i = 0; i < times; i++) {
+
+    let random = uprandom();
+    
     if (random >= 20 && random < 30) {
         Area1times++;
     } else if (random >= 30 && random < 60) {
@@ -82,11 +113,11 @@ for (let i = 0; i < times2; i++) {
 }
 
 console.log({
-    '20-30': Area1times/times2, 
-    '30-60': Area2times/times2,
-    '60-80': Area3times/times2,
-    '80-85': Area4times/times2,
-    '85-9999': Area5times/times2
+    '20-30': Area1times/times, 
+    '30-60': Area2times/times,
+    '60-80': Area3times/times,
+    '80-85': Area4times/times,
+    '85-9999': Area5times/times
 });     //similar to 0.05, 0.3, 0.22, 0.08, 0.35
 
 ```
